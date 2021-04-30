5bd3a0885db567f474c7ba10e4628cb3bdddb369
GoldAik <70778625+GoldAik@users.noreply.github.com>
2021-04-30 21:53:50 +0200

    base videos/imgs display

--- teczka/main/views.py
+++ teczka/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Video
 
 # Create your views here.
@@ -8,3 +8,25 @@ def index(response):
         'videos': Video.objects.all()
     }
     return render(response, "main/index.html", data)
+
+def search(request):
+    if request.method == 'POST':
+        phrase = request.POST.get('search', False)
+
+        if not phrase:
+            return render(request, 'main/search.html', {})
+
+        searched = Video.objects.filter(title__contains=phrase)
+        data = {
+            'results': searched,
+        }
+        return render(request, 'main/search.html', data)
+    else:
+        return render(request, 'main/search.html', {})
+
+
+def video(request, id):
+    video = Video.objects.filter(id=id).first()
+    if video is None:
+        return redirect('index') 
+    return render (request, 'main/video.html', {'video': video})
